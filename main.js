0af53ae34517187dae794f4306392808027b6e4d
JD <gtx2@protonmail.com>
2019-03-19 21:35:40 +0000

    start

---
+++ main.js
@@ -0,0 +1,11 @@
+let btn=document.getElementsByClassName('box');
+let parag=document.getElementsByTagName('p');
+
+console.log(btn[0]);
+
+
+btn.addEventListener('click',function(event){
+console.log(event);
+
+}
+)
